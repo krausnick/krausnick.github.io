82a206af6df716023da64ec6a7b99435f1acf122
krausnick <148052826+krausnick@users.noreply.github.com>
2023-10-24 15:05:45 +0200

    puuuuushhhhh

--- script.js
+++ script.js
@@ -80,11 +80,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 const nameCell = row.insertCell(0);
                 const idCell = row.insertCell(1);
 
-                // Erstellen Sie einen anklickbaren Link zum Benutzerprofil
+            // Erstellen eines anklickbaren Link zum Warenkorb
             const userLink = document.createElement('a');
             userLink.textContent = `${user.firstName} ${user.lastName}`;
             userLink.href = `Ergebnisse.html?userID=${user.id}`; // Leiten Sie zur Ergebnisse-Seite mit Benutzer-ID weiter
-            userLink.target = '_self'; // Öffnen Sie den Link in einem gleichen Tab
+            userLink.target = '_self'; // Öffnen des Links in einem gleichen Tab
 
             nameCell.appendChild(userLink);
             idCell.textContent = user.id;
@@ -92,82 +92,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function formatInput(input) {
-        // Überprüfen, ob die Eingabe vorhanden ist und mindestens einen Buchstaben enthält
-        if (input && input.length > 0) {
-            // Erster Buchstabe groß, Rest klein
-            return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-        }
-        // Wenn die Eingabe ungültig ist, gebe sie unverändert zurück
-        return input;
-    }
+  
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userID = urlParams.get('userID');
-
-    const nichtsimeinkaufswagen = document.getElementById('nichtsimeinkaufswagen');
-    const nichtsimeinkaufswagenbutton = document.getElementById('nichtsimeinkaufswagenbutton');
-
-    // Hier können Sie eine Fetch-Anfrage durchführen, um den Warenkorb dieses Benutzers abzurufen und auf der Seite anzuzeigen.
-    // Verwenden Sie die Benutzer-ID (userID) in der Anfrage-URL, um den entsprechenden Warenkorb abzurufen.
-
-    fetch(`https://dummyjson.com/users/${userID}/carts`)
-    .then(res => res.json())
-    .then(data => {
-        const cartContainer = document.getElementById('cart-container');
-        const carts = data.carts;
-        if(carts.length == 0) {
-            nichtsimeinkaufswagen.classList.remove('hidden');
-            nichtsimeinkaufswagenbutton.classList.remove('hidden');
-        } else {
-            // Hier fügen Sie den Wert der Benutzer-ID zum Titel hinzu
-        document.title = `ShoppingData.com - User ${userID}`;
-        carts.forEach(cart => {
-            const cartTable = document.createElement('table');
-            const cartItems = cart.products;
-
-            // Erstellen Sie die Tabellenüberschrift
-            const cartCaption = document.createElement('caption');
-            cartCaption.textContent = `Warenkorb ID: ${cart.id}`;
-            cartTable.appendChild(cartCaption);
-
-            // Erstellen Sie die Tabellenüberschriften (Header)
-            const cartHeader = cartTable.createTHead();
-            const headerRow = cartHeader.insertRow();
-            const headers = ['ID', 'Titel', 'Preis', 'Menge', 'Gesamt', 'Rabatt (%)', 'Rabattierter Preis'];
-            headers.forEach(headerText => {
-                const headerCell = document.createElement('th');
-                headerCell.textContent = headerText;
-                headerRow.appendChild(headerCell);
-            });
-
-            // Füllen Sie die Tabelle mit den Cart-Items
-            const cartBody = cartTable.createTBody();
-            cartItems.forEach(item => {
-                const itemRow = cartBody.insertRow();
-                const itemData = [item.id, item.title, item.price, item.quantity, item.total, item.discountPercentage, item.discountedPrice];
-                itemData.forEach(itemText => {
-                    const itemCell = itemRow.insertCell();
-                    itemCell.textContent = itemText;
-                });
-            });
-
-            // Fügen Sie die Tabelle zum Cart-Container hinzu
-            cartContainer.appendChild(cartTable);
-        });
+//Formatiere die Eingabe von Wohnsitzen so, dass Groß- und Kleinschreibung ignoriert werden!
+function formatInput(input) {
+    if (input && input.length > 0) {
+        return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
     }
-    })
-    .catch(error => {
-        console.error('Fehler beim Abrufen des Warenkorbs', error);
-    });
-
-    nichtsimeinkaufswagenbutton.addEventListener('click', function() {
-        window.location.href = 'index.html';
-    });
-        
+    return input;
+}
 
 
-
-});
