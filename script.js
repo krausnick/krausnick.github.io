27f856688915428fdf00f2829844b905ce24a0a8
krausnick <148052826+krausnick@users.noreply.github.com>
2023-10-30 20:53:58 +0100

    push 5.0

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 //Die Klasse "Router" 1:1 übernommen wie sie bereitgestellt wurde um SPA-Prinzip mit Router-Klasse umzusetzen!
+
 class Router {
     constructor(routes) {
         this._routes = routes;
@@ -34,8 +36,13 @@ class Router {
         route.show(matches);
     }
 }
-/* Diese Funktion zeigt die Suchleiste, den Suchbutton und einen Hinweistext auf der Startseite an, 
-indem sie die entsprechenden HTML-Elemente einblendet. */
+
+/* 
+    Diese Funktion zeigt die Suchleiste, den Suchbutton und einen Hinweistext auf der Startseite an, 
+    indem sie die entsprechenden HTML-Elemente einblendet. 
+*/
+
+
 function anzeigeStartSeite() {
     let searchInput = document.getElementById('state-input');
     let searchButton = document.getElementById('search-button');
@@ -45,21 +52,24 @@ function anzeigeStartSeite() {
     searchButton.style.display = 'block';
     hint.style.display = 'block';
 }
-/* In der setupStartPage-Funktion wird die Hauptfunktionalität für die Startseite implementiert, 
-wozu auch die Verlinkung der Benutzer in den Suchergebnissen gehört. Diese Verlinkung ermöglicht es Benutzern, 
-auf einen bestimmten Benutzer in den Suchergebnissen zu klicken, um weitere Details anzuzeigen:
 
-1. Die setupStartPage-Funktion reagiert auf das Klickereignis des Suchbuttons oder die Eingabe der Enter-Taste, 
-nachdem der Benutzer einen Suchbegriff eingegeben hat. Dies ist der Ausgangspunkt für die Suche nach Benutzern.
+/*  
+    In der setupStartPage-Funktion wird die Hauptfunktionalität für die Startseite implementiert, 
+    wozu auch die Verlinkung der Benutzer in den Suchergebnissen gehört. Diese Verlinkung ermöglicht es Benutzern, 
+    auf einen bestimmten Benutzer in den Suchergebnissen zu klicken, um weitere Details anzuzeigen:
 
-2. Bevor die Suche durchgeführt wird, wird die Funktion formatInput aufgerufen, um sicherzustellen, 
-dass die Eingabe des Benutzers formatiert ist. Diese Formatierung umfasst die Großschreibung des ersten Buchstabens 
-und die Kleinschreibung des restlichen Textes. Dadurch ist die Groß- und Kleinschreibung bei der Sucheingabe irrelevant. 
+    1. Die setupStartPage-Funktion reagiert auf das Klickereignis des Suchbuttons oder die Eingabe der Enter-Taste, 
+    nachdem der Benutzer einen Suchbegriff eingegeben hat. Dies ist der Ausgangspunkt für die Suche nach Benutzern.
 
-3. Nach der Formatierung wird eine API-Anfrage an den Server gesendet, um die Benutzer zu erhalten, 
-die dem eingegebenen Suchbegriff bzw. Wonhort entsprechen. 
-Die Ergebnisse werden in der displaySearchResults-Funktion verarbeitet.
+    2. Bevor die Suche durchgeführt wird, wird die Funktion formatInput aufgerufen, um sicherzustellen, 
+    dass die Eingabe des Benutzers formatiert ist. Diese Formatierung umfasst die Großschreibung des ersten Buchstabens 
+    und die Kleinschreibung des restlichen Textes. Dadurch ist die Groß- und Kleinschreibung bei der Sucheingabe irrelevant. 
+
+    3. Nach der Formatierung wird eine API-Anfrage an den Server gesendet, um die Benutzer zu erhalten, 
+    die dem eingegebenen Suchbegriff bzw. Wonhort entsprechen. 
+    Die Ergebnisse werden in der displaySearchResults-Funktion verarbeitet.
 */
+
 function setupStartPage() {
     console.log("Java-Script der Startseite geladen!");
     let searchInput = document.getElementById('state-input');
@@ -99,18 +109,20 @@ function setupStartPage() {
         anzeigeStartSeite();
     });
 }
-/* 
-4. In der displaySearchResults-Funktion werden die selektierten User in einer Tabelle, mit Name und UserID, 
-auf der Startseite angezeigt. 
-Für jeden Benutzer in den Suchergebnissen wird ein anklickbarer Link erstellt. 
-5. Diese Links sind so programmiert, dass sie auf die Ergebnisseite für den jeweiligen Benutzer verweisen. 
-Wenn ein Benutzer auf seinen Namen klickt, wird die URL geändert, und die Benutzer-ID wird als Parameter übergeben.
 
-Durch diese Verlinkung und die Verwendung der Benutzer-ID in der URL ermöglicht 
-die setupStartPage-Funktion die Navigation zu den Warenkorbdetails eines bestimmten Benutzers, 
-also auf die Main-Page "page-ergebnisse", 
-nachdem dieser in den Suchergebnissen ausgewählt wurde. 
+/* 
+    4. In der displaySearchResults-Funktion werden die selektierten User in einer Tabelle, mit Name und UserID, 
+    auf der Startseite angezeigt. 
+    Für jeden Benutzer in den Suchergebnissen wird ein anklickbarer Link erstellt. 
+    5. Diese Links sind so programmiert, dass sie auf die Ergebnisseite für den jeweiligen Benutzer verweisen. 
+    Wenn ein Benutzer auf seinen Namen klickt, wird die URL geändert, und die Benutzer-ID wird als Parameter übergeben.
+
+    Durch diese Verlinkung und die Verwendung der Benutzer-ID in der URL ermöglicht 
+    die setupStartPage-Funktion die Navigation zu den Warenkorbdetails eines bestimmten Benutzers, 
+    also auf die Main-Page "page-ergebnisse", 
+    nachdem dieser in den Suchergebnissen ausgewählt wurde. 
  */
+
 function displaySearchResults(users) {
     console.log("Die Suchergebnisse bzw. User werden angezeigt!");
     let resultsTable = document.getElementById('results-table').getElementsByTagName('tbody')[0];
@@ -169,41 +181,47 @@ function displaySearchResults(users) {
 
     }
 }
-//Durch diese Funktion wird bei der Eingabe des Wohnortes für die Suche der jeweiligen User, die Groß- und 
-//Kleinschreibung ignoriert!
+
+/*
+    Durch diese Funktion wird bei der Eingabe des Wohnortes für die Suche der jeweiligen User, die Groß- und 
+    Kleinschreibung ignoriert!
+*/
+
 function formatInput(input) {
     if (input && input.length > 0) {
         return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
     }
     return input;
 }
+
 /* 
-Die setupResultsPage-Funktion ist verantwortlich für die Darstellung der Ergebnisseite, 
-auf der die Warenkorbdetails eines ausgewählten Benutzers angezeigt werden. 
+    Die setupResultsPage-Funktion ist verantwortlich für die Darstellung der Ergebnisseite, 
+    auf der die Warenkorbdetails eines ausgewählten Benutzers angezeigt werden. 
 
-6. Die setupResultsPage-Funktion beginnt damit, die aktuelle URL der Seite zu erfassen. 
-Mithilfe einer Regulären Ausdrucks (Regex) wird die Benutzer-ID aus der URL extrahiert.
-Die Benutzer-ID ist ein wichtiger Parameter, da sie verwendet wird, 
-um die Warenkorbdetails des ausgewählten Benutzers abzurufen.
+    6. Die setupResultsPage-Funktion beginnt damit, die aktuelle URL der Seite zu erfassen. 
+    Mithilfe einer Regulären Ausdrucks (Regex) wird die Benutzer-ID aus der URL extrahiert.
+    Die Benutzer-ID ist ein wichtiger Parameter, da sie verwendet wird, 
+    um die Warenkorbdetails des ausgewählten Benutzers abzurufen.
 
-7. Nachdem die Benutzer-ID erfolgreich extrahiert wurde, überprüft die Funktion, 
-ob es für diesen Benutzer einen Warenkorb gibt. Wenn kein Warenkorb vorhanden ist, 
-wird eine Nachricht angezeigt, die dem Benutzer mitteilt, dass kein Warenkorb gefunden wurde.
-Dies geschieht durch das Ein- und Ausblenden entsprechender HTML-Elemente.
+    7. Nachdem die Benutzer-ID erfolgreich extrahiert wurde, überprüft die Funktion, 
+    ob es für diesen Benutzer einen Warenkorb gibt. Wenn kein Warenkorb vorhanden ist, 
+    wird eine Nachricht angezeigt, die dem Benutzer mitteilt, dass kein Warenkorb gefunden wurde.
+    Dies geschieht durch das Ein- und Ausblenden entsprechender HTML-Elemente.
 
-8. Wenn ein Warenkorb für den ausgewählten Benutzer vorhanden ist, 
-ruft die Funktion die Warenkorbdetails von der API ab. Dies erfolgt durch eine Fetch-Anfrage an den Server. 
-Die Benutzer-ID wird verwendet, um den entsprechenden Warenkorb zu identifizieren.
+    8. Wenn ein Warenkorb für den ausgewählten Benutzer vorhanden ist, 
+    ruft die Funktion die Warenkorbdetails von der API ab. Dies erfolgt durch eine Fetch-Anfrage an den Server. 
+    Die Benutzer-ID wird verwendet, um den entsprechenden Warenkorb zu identifizieren.
 
-9. Nach dem erfolgreichen Abrufen der Warenkorbdetails werden diese in einer Tabelle auf der Seite angezeigt. 
-Dafür wird die HTML-Tabelle aktualisiert, indem Zeilen für jedes Produkt im Warenkorb erstellt werden. 
-Die Produkttitel und Preise werden in den Tabellenzellen platziert.
+    9. Nach dem erfolgreichen Abrufen der Warenkorbdetails werden diese in einer Tabelle auf der Seite angezeigt. 
+    Dafür wird die HTML-Tabelle aktualisiert, indem Zeilen für jedes Produkt im Warenkorb erstellt werden. 
+    Die Produkttitel und Preise werden in den Tabellenzellen platziert.
 
-10. Gleiches Prinzip wie bei der displaySearchResults-Funktion: 
-Für jedes Item aus dem Warenkorb wird ein anklickbarer Link erstellt.
-Die ItemID des geklickten Items wird in URL übergeben und entsprechende 
-Main-Page "item-ergebnis" & setupItemPage-Funktion wird geswapped bzw. aufgerufen.
+    10. Gleiches Prinzip wie bei der displaySearchResults-Funktion: 
+    Für jedes Item aus dem Warenkorb wird ein anklickbarer Link erstellt.
+    Die ItemID des geklickten Items wird in URL übergeben und entsprechende 
+    Main-Page "item-ergebnis" & setupItemPage-Funktion wird geswapped bzw. aufgerufen.
 */
+
 function setupResultsPage() {
     console.log("Java-Script der Result-Seite geladen!");
     let einkaufswagencontainer = document.getElementById('einkaufswagen-container');
@@ -281,14 +299,16 @@ function setupResultsPage() {
 
 
 }
+
 /* 
-11. Item-ID wird aus der URL extrahiert nach gleichem Prinzip wie oben
+    11. Item-ID wird aus der URL extrahiert nach gleichem Prinzip wie oben
 
-12. Nachdem die Item-ID erfolgreich aus der URL extrahiert wurde, verwendet die Funktion diese, 
-um die Details des Elements (die Marke und die Kategorie) aus der API abzurufen.
+    12. Nachdem die Item-ID erfolgreich aus der URL extrahiert wurde, verwendet die Funktion diese, 
+    um die Details des Elements (die Marke und die Kategorie) aus der API abzurufen.
 
-13. Nach dem Abrufen der Elementdetails werden diese Informationen (Titel, Marke und Kategorie) auf der Seite angezeigt
+    13. Nach dem Abrufen der Elementdetails werden diese Informationen (Titel, Marke und Kategorie) auf der Seite angezeigt
 */
+
 function setupItemPage() {
     console.log("Java Script der Detailsicht des Items geladen!");
     let url = window.location.href 
@@ -326,10 +346,12 @@ function setupItemPage() {
         })
 
 }
+
 /*
-Notwendige Methode, dass entsprechend der Main-Content geswaped wird also Single-Page-Prinzip, 
-mit zusätzlichem Aufruf für jeweilige Funktion die den notwendigen Skript-Code für die jeweilige Main-Page läd!
+    Notwendige Methode, dass entsprechend der Main-Content geswaped wird also Single-Page-Prinzip, 
+    mit zusätzlichem Aufruf für jeweilige Funktion die den notwendigen Skript-Code für die jeweilige Main-Page läd!
 */
+
 window.addEventListener("load", () => {
     let swapContent = (id, title) => {
         document.querySelectorAll("main").forEach(mainElement => {
